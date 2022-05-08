657ff5b6b4e3308fd95a72f184c9f5d1af667490
Kang-Beom-Seo <bumsur@hanmail.net>
2022-05-08 16:31:27 +0000

    DPR prototype added

--- dpr/DPR.py
+++ dpr/DPR.py
@@ -23,12 +23,6 @@ from biencoder_data import BiEncoderSample, BiEncoderBatch
 from dpr_utils import Tensorizer
 from dpr_utils import CheckpointState
 
-logger = logging.getLogger(__name__)
-
-# TODO: it is only used by _select_span_with_token. Move them to utils
-rnd = random.Random(0)
-
-
 def dot_product_scores(q_vectors: T, ctx_vectors: T) -> T:
     """
     query vector에 대한 context vector의 score
@@ -75,154 +69,59 @@ class BiEncoder(nn.Module):
     ) -> Tuple[T, T, T]:
         sequence_output = None
         pooled_output = None
+        last_hidden_state = None
         hidden_states = None
         if ids is not None:
             if fix_encoder:
                 with torch.no_grad():
-                    sequence_output, pooled_output, hidden_states = sub_model(
-                        ids,
-                        segments,
-                        attn_mask
-                    )
+                    last_hidden_state, pooled_output = sub_model(
+                        input_ids=ids,
+                        token_type_ids=segments,
+                        attention_mask=attn_mask
+                    ).to_tuple()
 
                 if sub_model.training:
                     sequence_output.requires_grad_(requires_grad=True)
                     pooled_output.requires_grad_(requires_grad=True)
             else:
-                sequence_output, pooled_output, hidden_states = sub_model(
-                    ids,
-                    segments,
-                    attn_mask
-                )
+                last_hidden_state, pooled_output = sub_model(
+                    input_ids=ids,
+                    token_type_ids=segments,
+                    attention_mask=attn_mask
+                ).to_tuple()
 
-        return sequence_output, pooled_output, hidden_states
+        return last_hidden_state, pooled_output
 
     def forward(
         self,
-        question_ids: T,
-        question_segments: T,
-        question_attn_mask: T, # 여기까지는 question
-        context_ids: T,
-        ctx_segments: T,
-        ctx_attn_mask: T, # 여기까지는 context
+        questions_input_ids: T,
+        questions_token_type_ids: T,
+        questions_attention_mask: T, # 여기까지는 question
+        passages_input_ids: T,
+        passages_token_type_ids: T,
+        passages_attention_mask: T, # 여기까지는 context
+        labels=None, #이건 안쓰임. compute_metrics를 사용하기 위해 넣음.
         encoder_type: str = None
     ) -> Tuple[T, T]:
         q_encoder = self.question_model if encoder_type is None or encoder_type == "question" else self.ctx_model
-        _q_seq, q_pooled_out, _q_hidden = self.get_representation(
-            q_encoder,
-            question_ids,
-            question_segments,
-            question_attn_mask,
-            self.fix_q_encoder
+        q_hidden, q_pooled_out = self.get_representation(
+            sub_model=q_encoder,
+            ids=questions_input_ids,
+            segments=questions_token_type_ids,
+            attn_mask=questions_attention_mask,
+            fix_encoder=self.fix_q_encoder
         )
 
         ctx_encoder = self.ctx_model if encoder_type is None or encoder_type == "ctx" else self.question_model
-        _ctx_seq, ctx_pooled_out, _ctx_hidden = self.get_representation(
-            ctx_encoder,
-            context_ids,
-            ctx_segments,
-            ctx_attn_mask,
-            self.fix_ctx_encoder
+        ctx_hidden, ctx_pooled_out = self.get_representation(
+            sub_model=ctx_encoder,
+            ids=passages_input_ids,
+            segments=passages_token_type_ids,
+            attn_mask=passages_attention_mask,
+            fix_encoder=self.fix_ctx_encoder
         )
 
         return q_pooled_out, ctx_pooled_out
-
-    def create_biencoder_input(
-        self,
-        samples: List[BiEncoderSample],
-        tensorizer: Tensorizer,
-        insert_title: bool,
-        shuffle: bool = True,
-        shuffle_positives: bool = False,
-        hard_neg_fallback: bool = True,
-        query_token: str = None,
-    ) -> BiEncoderBatch:
-        """
-        biencoder training tuple의 batch를 생성한다.
-        :param samples: batch에 들어갈 BiEncoderSample들의 list
-        :param tensorizer: text sequence에서 model의 input tensor를 생성하기 위한 구성요소
-        :param insert_title: context sequence들의 맨 앞에 title을 추가할 지 여부
-        :param shuffle: negative passage pool을 shuffle함
-        :param shuffle_positives: positive passage pool을 shuffle함
-        :return: BiEncoderBatch tuple
-        """
-        question_tensors = []
-        ctx_tensors = []
-        positive_ctx_indices = []
-        hard_neg_ctx_indices = []
-
-        for sample in samples:
-            # ctx+ & [ctx-] composition
-            # as of now, take the first(gold) ctx+ only
-
-            if shuffle and shuffle_positives:
-                positive_ctxs = sample.positive_passages
-                positive_ctx = positive_ctxs[np.random.choice(len(positive_ctxs))]
-            else:
-                positive_ctx = sample.positive_passages[0]
-
-            neg_ctxs = sample.negative_passages
-            hard_neg_ctxs = sample.hard_negative_passages
-            question = sample.query
-
-            if shuffle:
-                random.shuffle(neg_ctxs)
-                random.shuffle(hard_neg_ctxs)
-
-            if hard_neg_fallback and len(hard_neg_ctxs) == 0:
-                hard_neg_ctxs = neg_ctxs[0:num_hard_negatives]
-
-            neg_ctxs = neg_ctxs[0:num_other_negatives]
-            hard_neg_ctxs = hard_neg_ctxs[0:num_hard_negatives]
-
-            all_ctxs = [positive_ctx] + neg_ctxs + hard_neg_ctxs
-            hard_negatives_start_idx = 1
-            hard_negatives_end_idx = 1 + len(hard_neg_ctxs)
-
-            current_ctxs_len = len(ctx_tensors)
-
-            sample_ctxs_tensors = [
-                tensorizer.text_to_tensor(ctx.text, title=ctx.title if (insert_title and ctx.title) else None)
-                for ctx in all_ctxs
-            ]
-
-            ctx_tensors.extend(sample_ctxs_tensors)
-            positive_ctx_indices.append(current_ctxs_len)
-            hard_neg_ctx_indices.append(
-                [
-                    i
-                    for i in range(
-                        current_ctxs_len + hard_negatives_start_idx,
-                        current_ctxs_len + hard_negatives_end_idx,
-                    )
-                ]
-            )
-
-            if query_token:
-                # TODO: tmp workaround for EL, remove or revise
-                if query_token == "[START_ENT]":
-                    query_span = _select_span_with_token(question, tensorizer, token_str=query_token)
-                    question_tensors.append(query_span)
-                else:
-                    question_tensors.append(tensorizer.text_to_tensor(" ".join([query_token, question])))
-            else:
-                question_tensors.append(tensorizer.text_to_tensor(question))
-
-        ctxs_tensor = torch.cat([ctx.view(1, -1) for ctx in ctx_tensors], dim=0)
-        questions_tensor = torch.cat([q.view(1, -1) for q in question_tensors], dim=0)
-
-        ctx_segments = torch.zeros_like(ctxs_tensor)
-        question_segments = torch.zeros_like(questions_tensor)
-
-        return BiEncoderBatch(
-            questions_tensor,
-            question_segments,
-            ctxs_tensor,
-            ctx_segments,
-            positive_ctx_indices,
-            hard_neg_ctx_indices,
-            "question",
-        )
 
     def load_state(self, saved_state: CheckpointState, strict: bool = True):
         # TODO: make a long term HF compatibility fix
@@ -240,7 +139,6 @@ class BiEncoderNllLoss(object):
         self,
         q_vectors: T,
         ctx_vectors: T,
-        positive_idx_per_question: list,
         loss_scale: float = None,
     ) -> Tuple[T, int]:
         """
@@ -255,6 +153,7 @@ class BiEncoderNllLoss(object):
             scores = scores.view(q_num, -1)
 
         softmax_scores = F.log_softmax(scores, dim=1)
+        positive_idx_per_question = [i for i in range(q_num)]
 
         loss = F.nll_loss(
             softmax_scores, # prediction probability
@@ -262,13 +161,13 @@ class BiEncoderNllLoss(object):
             reduction="mean",
         )
 
-        max_score, max_idxs = torch.max(softmax_scores, 1) # top-1 score 및 index
-        correct_predictions_count = (max_idxs == torch.tensor(positive_idx_per_question).to(max_idxs.device)).sum()
+        # max_score, max_idxs = torch.max(softmax_scores, 1) # top-1 score 및 index
+        # correct_predictions_count = (max_idxs == torch.tensor(positive_idx_per_question).to(max_idxs.device)).sum()
 
         if loss_scale:
             loss.mul_(loss_scale)
 
-        return loss, correct_predictions_count
+        return loss, softmax_scores
 
     @staticmethod
     def get_scores(q_vector: T, ctx_vectors: T) -> T:
---
+++ dpr/trainer_dpr.py
@@ -0,0 +1,143 @@
+# coding=utf-8
+# Copyright 2020 The HuggingFace Team All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Dense Passage Retriever의 training을 위한 'Trainer'의 subclass 코드 입니다.
+"""
+
+from transformers import Trainer, EvalPrediction
+from transformers.trainer_utils import PredictionOutput
+from torch.utils.data import DataLoader
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase
+from dataclasses import dataclass
+from typing import Union, Optional, List, Dict
+from transformers.file_utils import PaddingStrategy
+import datasets
+import torch
+from DPR import BiEncoderNllLoss
+
+def compute_metrics(p: EvalPrediction):
+        predictions, label_ids = p
+        max_score, max_idxs = torch.max(predictions, 1) # top-1 score 및 index
+        q_num = len(predictions)
+        positive_idx_per_question = [i for i in range(q_num)]
+        correct_predictions_count = (max_idxs == torch.tensor(positive_idx_per_question).to(max_idxs.device)).sum()
+        answer_dict = {'correct_count' : correct_predictions_count}
+        return answer_dict
+
+# Bi-Encoder의 input을 만들기 위한 data
+@dataclass
+class DataCollatorWithPaddingForDPR:
+    """
+    Data collator that will dynamically pad the inputs received.
+    Args:
+        tokenizer (:class:`~transformers.PreTrainedTokenizer` or :class:`~transformers.PreTrainedTokenizerFast`):
+            The tokenizer used for encoding the data.
+        padding (:obj:`bool`, :obj:`str` or :class:`~transformers.file_utils.PaddingStrategy`, `optional`, defaults to :obj:`True`):
+            Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
+            among:
+            * :obj:`True` or :obj:`'longest'`: Pad to the longest sequence in the batch (or no padding if only a single
+              sequence if provided).
+            * :obj:`'max_length'`: Pad to a maximum length specified with the argument :obj:`max_length` or to the
+              maximum acceptable input length for the model if that argument is not provided.
+            * :obj:`False` or :obj:`'do_not_pad'` (default): No padding (i.e., can output a batch with sequences of
+              different lengths).
+        max_length (:obj:`int`, `optional`):
+            Maximum length of the returned list and optionally padding length (see above).
+        pad_to_multiple_of (:obj:`int`, `optional`):
+            If set will pad the sequence to a multiple of the provided value.
+            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
+            7.5 (Volta).
+    """
+
+    tokenizer: PreTrainedTokenizerBase
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+
+    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
+        # 여기서, question 및 passage를 나누는 역할을 한다.
+        question_features = []
+        passages_features = []
+        batch = {}
+        labels = []
+        for feature in features:
+            passages_feature = {}
+            question_feature = {}
+            passages_feature['input_ids'] = feature.pop('passages_input_ids')
+            passages_feature['token_type_ids'] = feature.pop('passages_token_type_ids')
+            passages_feature['attention_mask'] = feature.pop('passages_attention_mask')
+
+            question_feature['input_ids'] = feature.pop('questions_input_ids')
+            question_feature['token_type_ids'] = feature.pop('questions_token_type_ids')
+            question_feature['attention_mask'] = feature.pop('questions_attention_mask')
+
+            question_features.append(question_feature)
+            passages_features.append(passages_feature)
+            labels.append(-1)
+        batch['labels'] = torch.tensor(labels)
+        question_batch = self.tokenizer.pad(
+            question_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+
+        if "label" in question_batch:
+            question_batch["labels"] = question_batch["label"]
+            del question_batch["label"]
+        if "label_ids" in question_batch:
+            question_batch["labels"] = question_batch["label_ids"]
+            del question_batch["label_ids"]
+        
+        passage_batch = self.tokenizer.pad(
+            passages_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+
+        if "label" in passage_batch:
+            passage_batch["labels"] = passage_batch["label"]
+            del passage_batch["label"]
+        if "label_ids" in passage_batch:
+            passage_batch["labels"] = passage_batch["label_ids"]
+            del passage_batch["label_ids"]
+
+        for key in ['input_ids', 'token_type_ids', 'attention_mask']:
+            batch['questions_' + key] = question_batch[key]
+            batch['passages_' + key] = passage_batch[key]
+
+        return batch
+
+# Huggingface의 Trainer를 상속받아 DPR을 위한 Trainer를 생성합니다.
+class DensePassageRetrievalTrainer(Trainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.pop('labels')
+        q_outputs, p_outputs = model(**inputs)
+        loss_fct = BiEncoderNllLoss()
+        loss, outputs = loss_fct.calc(q_outputs, p_outputs)
+
+        # trainer에서 eval 계산 후 첫 항을 제외하는 코드가 있어서 이렇게 구현하였음.
+        # trainer.py의 2033줄 logits = outputs[1:] 참조
+        if return_outputs:
+            outputs_shape = outputs.shape[0]
+            outputs = torch.cat([torch.zeros([1, outputs_shape]).to(outputs.device), outputs], dim=0)
+
+        return (loss, outputs) if return_outputs else loss
